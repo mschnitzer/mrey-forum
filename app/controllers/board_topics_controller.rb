432e7b2ced7ae7a29796ccac22c822de5f2ae4ce
Manuel Schnitzer <webmaster@mschnitzer.de>
2019-11-26 09:54:41 +0100

    save

--- app/controllers/board_topics_controller.rb
+++ app/controllers/board_topics_controller.rb
@@ -1,19 +1,45 @@
 class BoardTopicsController < ApplicationController
+  # GET /boards/:id/topics/new
   def new
+    Board.find(params[:id])
+
     @board_topic = BoardTopic.new
+    @board_topic_post = BoardTopicPost.new
   end
 
+  # POST /boards/:id/topics/new
   def create
-    params = board_topic_create_params
-    byebug
+    board = Board.find(params[:id])
 
-    @board_topic = BoardTopic.new
+    ApplicationRecord.transaction do
+      @board_topic = BoardTopic.new(
+        board:    board,
+        title:    params[:board_topic][:title],
+        username: params[:board_topic][:username]
+      )
+
+      @board_topic_post = BoardTopicPost.new(
+        username: params[:board_topic][:username],
+        text:     params[:board_topic][:board_topic_post][:text]
+      )
+
+      if @board_topic.save
+        @board_topic_post.board_topic = @board_topic
+
+        if @board_topic_post.save
+          @board_topic = BoardTopic.new
+          @board_topic_post = BoardTopicPost.new
+
+          flash.now[:ok] = 'saved'
+        else
+          flash.now[:error] = @board_topic_post.errors.full_messages.to_sentence
+          raise ActiveRecord::Rollback
+        end
+      else
+        flash.now[:error] = @board_topic.errors.full_messages.to_sentence
+      end
+    end
+
     render :new
-  end
-
-  private
-
-  def board_topic_create_params
-    params.require(:board_topic).permit(:username, :title, board_topic_posts: [:text])
   end
 end
