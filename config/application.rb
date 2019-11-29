9c0b193b9c43058845d8aeca31bc72942a8c31d8
Manuel Schnitzer <webmaster@mschnitzer.de>
2019-11-29 10:30:45 +0100

    Establish websocket connection

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Forum
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_cable.mount_path = '/cable'
+    config.generators.system_tests = nil
   end
 end
